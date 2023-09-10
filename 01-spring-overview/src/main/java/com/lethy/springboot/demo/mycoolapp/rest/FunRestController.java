2ba53554df6016d0671c6d3cbcba27d23fc4c61a
ellethykhaled <ellethykhaled2@gmail.com>
2023-09-10 18:21:48 +0300

    Custom application properties

--- 01-spring-overview/src/main/java/com/lethy/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 01-spring-overview/src/main/java/com/lethy/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,23 @@
 package com.lethy.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    // New endpoint for custom properties
+    @GetMapping("/teamInfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
 
     // expose "/" that returns "Hello World"
     @GetMapping("/")
