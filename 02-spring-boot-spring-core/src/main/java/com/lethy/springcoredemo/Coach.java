4a59401ab945e2a5b03bd2c27f59265e7d6b24ec
ellethykhaled <ellethykhaled2@gmail.com>
2023-09-10 20:00:49 +0300

    Dependency Injection

---
+++ 02-spring-boot-spring-core/src/main/java/com/lethy/springcoredemo/Coach.java
@@ -0,0 +1,5 @@
+package com.lethy.springcoredemo;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ 02-spring-boot-spring-core/src/main/java/com/lethy/springcoredemo/DemoController.java
@@ -0,0 +1,21 @@
+package com.lethy.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach myCoach;
+
+    @Autowired
+    DemoController(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
