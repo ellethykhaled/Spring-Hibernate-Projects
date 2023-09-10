383afb5354622459de2bb5852ff778cd6528c34c
ellethykhaled <ellethykhaled2@gmail.com>
2023-09-10 21:26:20 +0300

    Bean configuration

--- 02-spring-boot-spring-core/src/main/java/com/lethy/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core/src/main/java/com/lethy/springcoredemo/rest/DemoController.java
@@ -12,7 +12,7 @@ public class DemoController {
     private Coach coach;
 
     @Autowired
-    DemoController(@Qualifier("cricketCoach") Coach coach) {
+    DemoController(@Qualifier("aquatic") Coach coach) {
         System.out.println("In constructor: " + getClass().getSimpleName());
         this.coach = coach;
     }
