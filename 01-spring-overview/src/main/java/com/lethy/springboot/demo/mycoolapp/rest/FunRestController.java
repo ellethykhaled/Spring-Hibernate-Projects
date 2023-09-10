13a8803f2be53b38884e86d830859127503ef59f
ellethykhaled <ellethykhaled2@gmail.com>
2023-09-10 17:18:26 +0300

    Spring actuators exposed

--- 01-spring-overview/src/main/java/com/lethy/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 01-spring-overview/src/main/java/com/lethy/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,6 +1,5 @@
 package com.lethy.springboot.demo.mycoolapp.rest;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,12 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class FunRestController {
 
     // expose "/" that returns "Hello World"
-    @Value("${developer.name}")
-    String developerName;
-
     @GetMapping("/")
     public String sayHello() {
-        return "Hello " + developerName + "!";
+        return "Hello World!";
     }
 
     // expose a new endpoint for "workout"
