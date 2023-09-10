fe94b846846d1c85ebbeb131598ea5565a440d6f
ellethykhaled <ellethykhaled2@gmail.com>
2023-09-10 17:07:08 +0300

    Renaming project

--- 01-spring-overview/src/main/java/com/lethy/springboot/demo/mycoolapp/MyCoolAppApplication.java
+++ 01-spring-overview/src/main/java/com/lethy/springboot/demo/mycoolapp/MyCoolAppApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MycoolappApplication {
+public class MyCoolAppApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(MycoolappApplication.class, args);
+		SpringApplication.run(MyCoolAppApplication.class, args);
 	}
 
 }
