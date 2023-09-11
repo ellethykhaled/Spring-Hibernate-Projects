e972b6b12bf6d2dd9049bd758a20f64a4597b051
ellethykhaled <ellethykhaled2@gmail.com>
2023-09-11 10:54:27 +0300

    Student DAO

---
+++ 03-spring-boot-hibernate-jpa/01-crud-demo/src/main/java/com/lethy/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package com.lethy.cruddemo.dao;
+
+import com.lethy.cruddemo.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student theStudent);
+}
---
+++ 03-spring-boot-hibernate-jpa/01-crud-demo/src/main/java/com/lethy/cruddemo/dao/StudentDAOImpl.java
@@ -0,0 +1,25 @@
+package com.lethy.cruddemo.dao;
+
+import com.lethy.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO {
+
+    EntityManager entityManager;
+
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+
+    }
+
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent);
+    }
+}
