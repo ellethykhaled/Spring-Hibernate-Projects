4d80114467c0aec85b681678cabd15629c5bbda1
ellethykhaled <ellethykhaled2@gmail.com>
2023-09-13 18:22:38 +0300

    Getting students

---
+++ 04-rest-api/src/main/java/com/lethy/restapi/rest/StudentRestController.java
@@ -0,0 +1,34 @@
+package com.lethy.restapi.rest;
+
+
+import com.lethy.restapi.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+
+    List<Student> students;
+
+    @PostConstruct
+    void populateStudents() {
+        students = new ArrayList<>();
+
+        students.add(new Student("Poornima", "Patel"));
+        students.add(new Student("Mario", "Rossi"));
+        students.add(new Student("Mary", "Smith"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return students;
+    }
+}
