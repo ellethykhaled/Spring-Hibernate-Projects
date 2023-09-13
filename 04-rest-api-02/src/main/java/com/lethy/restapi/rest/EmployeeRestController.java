e3604f141109c446ea9ea9671f18fa939a9dcdc6
ellethykhaled <ellethykhaled2@gmail.com>
2023-09-13 23:18:14 +0300

    Update and delte

--- 04-rest-api-02/src/main/java/com/lethy/restapi/rest/EmployeeRestController.java
+++ 04-rest-api-02/src/main/java/com/lethy/restapi/rest/EmployeeRestController.java
@@ -3,9 +3,7 @@ package com.lethy.restapi.rest;
 import com.lethy.restapi.entity.Employee;
 import com.lethy.restapi.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,40 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    Employee getEmployee(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    Employee addEmployee(@RequestBody Employee employee) {
+        employee.setId(0);
+
+        return employeeService.save(employee);
+    }
+
+    @PutMapping("/employees")
+    Employee updateEmployee(@RequestBody Employee employee) {
+        return employeeService.save(employee);
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+
+        Employee employee = employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
     }
 }
