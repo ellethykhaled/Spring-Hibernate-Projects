4a1c4f09de6c009cc16fab868446a75e3c874b73
ellethykhaled <ellethykhaled2@gmail.com>
2023-09-19 02:21:51 +0300

    Updating employee

--- 07-spring-boot-spring-mvc-crud/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,10 +6,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +33,19 @@ public class EmployeeController {
 		theModel.addAttribute("employees", theEmployees);
 
 		return "employees/list-employees";
+	}
+
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int employeeId, Model theModel) {
+
+		// get the employee from the service
+		Employee theEmployee = employeeService.findById(employeeId);
+
+		// set the employee in the model to prepopulate the form
+		theModel.addAttribute("employee", theEmployee);
+
+		// send over to our form
+		return "employees/employee-form";
 	}
 
 	@GetMapping("/showFormForAdd")
